1de934aa44a52342ad7fd459a537e2ad6d6e1aed
若寒 <82257841+zzw2016@users.noreply.github.com>
2022-03-25 15:09:22 +0800

    这是我的第一篇文章。文章使用 GitHub 管理，并通过 GitHub Actions 自动构建与发布！
    [幽灵公主剧照](./images/Mononoke_Hime.jpg)

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": true,
+    "repo": "zzw2016/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
